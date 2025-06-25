12100d8f064445a540038248caacbda09b48831e
code3743 <jota.lopez@correounivalle.edu.co>
2025-06-25 06:35:17 -0500

    Add UserMapper and UserResponse model for user data transformation

---
+++ lib/infraestructure/mappers/user_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:gtu_mobile/domain/entities/user.dart';
+import 'package:gtu_mobile/infraestructure/models/user_response.dart';
+
+class UserMapper {
+  static UserModel toModel(User user) {
+    return UserModel(name: user.name, email: user.email, userId: user.id);
+  }
+
+  static User toEntity(UserModel userModel) {
+    return User(userModel.userId, userModel.name, userModel.email);
+  }
+}
