e9f91b3e10e4bf9c5e22ddf248522eca770e8343
code3743 <jota.lopez@correounivalle.edu.co>
2025-06-25 06:34:15 -0500

    Implement authentication repository and data source with sign-in, sign-out, and sign-up functionalities

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,7 +1,8 @@
 import 'package:gtu_mobile/domain/entities/user.dart';
 
 abstract class AuthRepository {
-  Future<User> signInWithEmailAndPassword(String email, String password);
+  Future<void> signInWithEmailAndPassword(String email, String password);
   Future<void> signOut();
-  Future<void> signUp(User user);
+  Future<void> signUp(String name, String email, String password);
+  Future<User> getCurrentUser();
 }
---
+++ lib/infraestructure/datasources/api/auth_datasource_remote.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:gtu_mobile/config/constants/api.dart';
+import 'package:gtu_mobile/config/networks/http_error.dart';
+import 'package:gtu_mobile/infraestructure/models/user_response.dart';
+
+class AuthDatasourceRemote {
+  final Dio _dio;
+  AuthDatasourceRemote(this._dio);
+
+  Future<UserModel> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final response = await _dio.post(
+        Api.login,
+        data: {'email': email, 'password': password},
+      );
+      return UserResponse.fromJson(response.data).data;
+    } catch (e) {
+      if (e is DioException) {
+        throw handleDioError(e);
+      }
+      rethrow;
+    }
+  }
+
+  Future<void> signOut() {
+    throw UnimplementedError();
+  }
+
+  Future<UserModel> signUp(String name, String email, String password) async {
+    try {
+      final response = await _dio.post(
+        Api.register,
+        data: {'name': name, 'email': email, 'password': password},
+      );
+      return UserResponse.fromJson(response.data).data;
+    } catch (e) {
+      if (e is DioException) {
+        throw handleDioError(e);
+      }
+      rethrow;
+    }
+  }
+}
