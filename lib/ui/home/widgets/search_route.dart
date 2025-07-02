3e3b29b823b011856fb1a00244f1d4bc9d6a618c
code3743 <jota.lopez@correounivalle.edu.co>
2025-07-02 05:06:49 -0500

    feat: Add Google Places autocomplete functionality to search route

--- lib/ui/home/widgets/search_route.dart
+++ lib/ui/home/widgets/search_route.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_places_autocomplete_text_field/google_places_autocomplete_text_field.dart';
+import 'package:gtu_mobile/config/routes/app_router.dart';
+
+const _googleAPIKey = 'API-KEY'; // Replace with your actual Google API key
 
 class SearchRoute extends StatelessWidget {
   const SearchRoute({super.key});
@@ -24,6 +29,47 @@ class SearchRoute extends StatelessWidget {
               ),
             ],
           ),
+          child: GooglePlacesAutoCompleteTextFormField(
+            googleAPIKey: _googleAPIKey,
+            decoration: const InputDecoration(
+              hintText: '¿A dónde quieres ir?',
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 10,
+              ),
+              border: InputBorder.none,
+            ),
+            countries: ['co'],
+            languageCode: 'es',
+
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Por favor ingresa un texto';
+              }
+              return null;
+            },
+            // proxyURL: _yourProxyURL,
+            maxLines: 1,
+            overlayContainerBuilder: (child) => Material(
+              elevation: 1.0,
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              child: child,
+            ),
+            fetchCoordinates: true,
+            onPlaceDetailsWithCoordinatesReceived: (prediction) {
+              final latStr = prediction.lat?.toString() ?? '0.0';
+              final lngStr = prediction.lng?.toString() ?? '0.0';
+              final lat = double.tryParse(latStr) ?? 0.0;
+              final lng = double.tryParse(lngStr) ?? 0.0;
+              if (lat == 0.0 || lng == 0.0) return;
+              context.pushNamed(AppRouterName.nearbyRoutes, extra: [lat, lng]);
+            },
+            onSuggestionClicked: (prediction) {},
+
+            minInputLength: 3,
+          ),
+          /*
           child: TextFormField(
             readOnly: true,
             onTap: () {},
@@ -33,7 +79,7 @@ class SearchRoute extends StatelessWidget {
               border: InputBorder.none,
               prefixIcon: Icon(Icons.search),
             ),
-          ),
+          ), */
         ),
       ),
     );
